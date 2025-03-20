bf4ff5b865bbf2486fe0fc49d0111964a8c48c1f
sambarvadai <ani.csekaran@gmail.com>
2025-03-19 20:02:53 -0400

    Added persistence in local storage for Dark mode. Fixed a z-index issue with nav links

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
-var isDarkMode = false;
+var isDarkMode = localStorage.getItem("darkMode")==="true"? true: false;
 
 window.onload = function(){
     var mybtn = document.getElementById("toggle-txt");
     function toggleDarkMode() {
-        if(!isDarkMode)
+        if(isDarkMode)
         {
             //Change to light mode:
-            isDarkMode = true;
+
             mybtn.innerHTML = "Light Mode";
             document.documentElement.style.backgroundColor = "#192929";
             document.body.style.color = "#F7F7F7"
@@ -14,15 +14,19 @@ window.onload = function(){
         }
         else
         {   
-            isDarkMode = false;
             //change to dark mode:
             mybtn.innerHTML = "Dark Mode";
             document.documentElement.style.backgroundColor="#F7F7F7";
             document.body.style.color = "#192929";
             document.documentElement.style.setProperty("--underline-default","#192929");
-
             
         }
     }
-    mybtn.addEventListener("click", toggleDarkMode);
+    function applyDarkMode(){
+        isDarkMode = !isDarkMode;
+        localStorage.setItem("darkMode",isDarkMode.toString());
+        toggleDarkMode();
+    }
+    mybtn.addEventListener("click", applyDarkMode);
+    toggleDarkMode();
 };
