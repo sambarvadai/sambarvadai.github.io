bde562d7ee145081a77b1ecf27af198303216b6b
sambarvadai <ani.csekaran@gmail.com>
2025-04-18 01:27:01 -0400

    Minor bug fixes

--- assets/js/currentMusic.js
+++ assets/js/currentMusic.js
@@ -1,15 +1,13 @@
 const currentPlaying = document.getElementById("music-cur");
 async function getCurrentMusic(){
     const res = await fetch("https://portfolio-backend-jt4c.onrender.com/api/spotify/now-playing");
-    const data = await res.json();
     if(res.status === 204){
         currentPlaying.innerHTML = "Currently Listening to: The sound of silence...";
         return;
     }
-    else
-    {
-        currentPlaying.innerHTML = `Currently Listening to: ${data.song}`;
-    }
+    const data = await res.json();
+    currentPlaying.innerHTML = `Currently Listening to: ${data.song}`;
+    
 }
 getCurrentMusic();
 setInterval(getCurrentMusic, 30000); // Update every 30 seconds
